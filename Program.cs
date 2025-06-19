bc8f92424a17bc95a2fa20dcbed089ebe2e371e4
SBC <seobc789@gmail.com>
2025-06-19 11:40:51 +0900

    [Feature] 토글 스위치 테스트
    - IoT Hub 디바이스 변경
    - 토글 스위치 체크

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices;
 using Microsoft.Azure.Devices.Client;
 using Microsoft.Azure.Devices.Shared;
 using System.Device.Gpio;
@@ -10,33 +10,41 @@ class Program
     private static bool lastState = false;
     private static GpioController gpio = new GpioController();
     private static int toggleSwitchPin = 4;
+    private static PinValue pinValue;
     
-    static string connectionSendString = "HostName=Uni12TwinProTest.azure-devices.net;DeviceId=azure-samples-test;SharedAccessKey=4GPoU+qynj2+RZnmzlFti2U5yUEJ7nRL/KvJpiUtsno=";
+    static string connectionSendString = "HostName=Uni12TwinPro.azure-devices.net;DeviceId=TestDevice;SharedAccessKey=tYGn6+N1iCwjiGVaM8oJp3HzlLinx0W6w0bHoMw5HOo=";
     // static string connectionRecvString = "HostName=Uni12TwinProTest.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=Ez/vrnK5xfmYrosJCMRRkR7wuIDZGSV/BAIoTNvpoiU=";
     
     // static RegistryManager registryManager;
     static async Task Main(string[] args)
     {
         // registryManager = RegistryManager.CreateFromConnectionString(connectionRecvString);
-        
         try
         {
             deviceClient = DeviceClient.CreateFromConnectionString(connectionSendString);
 
             gpio.OpenPin(toggleSwitchPin, PinMode.Input);
-            
+
+            Console.WriteLine($"[{DateTime.Now}] current state : {currentState}");
+            pinValue = gpio.Read(toggleSwitchPin);
+            Console.WriteLine($"[{DateTime.Now}] pin value : {pinValue}");
+
             bool running = true;
             while (running)
             {
-                var key = Console.ReadKey(true).Key;
-                if (key == ConsoleKey.Escape)
-                {
-                    running = false;
-                }
+                pinValue = gpio.Read(toggleSwitchPin);
 
-                PinValue value = gpio.Read(toggleSwitchPin);
-                await SendToggleStateAsync(value);
+                await SendToggleStateAsync();
                 await Task.Delay(50);
+
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey(true).Key;
+                    if (key == ConsoleKey.Escape)
+                    {
+                        running = false;
+                    }
+                }
             }
         }
         catch (Exception ex)
@@ -45,6 +53,7 @@ class Program
         }
         finally
         {
+            Console.WriteLine($"program finish");
             if (deviceClient != null)
             {
                 await deviceClient.CloseAsync();
@@ -58,27 +67,29 @@ class Program
     //     
     // }
 
-    static async Task SendToggleStateAsync(PinValue value)
+    static async Task SendToggleStateAsync()
     {
-        if (value == PinValue.High)
+        //Console.WriteLine($"[{DateTime.Now}] SendToggleStateAsync");
+
+        if (pinValue == PinValue.High)
         {
             currentState = true;
         }
-        else if (value == PinValue.Low)
+        else if (pinValue == PinValue.Low)
         {
             currentState = false;
         }
-        
+
         if (currentState == lastState)
         {
             return;
         }
-        
+
         lastState = currentState;
+        Console.WriteLine($"[{DateTime.Now}] current state: {currentState}");
         TwinCollection reportedProperties = new TwinCollection();
         reportedProperties["toggleState"] = currentState;
 
         await deviceClient.UpdateReportedPropertiesAsync(reportedProperties);
-        Console.WriteLine($"[{DateTime.Now}] 상태 업데이트: {currentState}");
     }
 }
